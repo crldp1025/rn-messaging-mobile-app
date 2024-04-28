5a83e3ef4d6bb8b9eab95dd0612c0177aa9b5a34
Carlo <crldp1025@gmail.com>
2024-04-28 12:46:03 +0800

    Installed react navigation bottom stack Bottom Tab Navigator setup Added contacts, chats and settings screen Added reusable components

---
+++ src/screens/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '../components/Text';
+
+const Chat = () => {
+  return (
+    <View>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+};
+
+export default Chat;
---
+++ src/screens/Contacts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '../components/Text';
+
+const Contacts = () => {
+  return (
+    <View>
+      <Text>Contacts</Text>
+    </View>
+  );
+};
+
+export default Contacts;
