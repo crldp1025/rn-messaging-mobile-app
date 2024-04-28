31b5c774e219cdf754b33f50d1ba778a52c96073
Carlo <crldp1025@gmail.com>
2024-04-28 12:20:44 +0800

    Initial setup Packages installationm Folder structure

---
+++ src/themes/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: '#d04848',
+  white: '#fff',
+  black: '#000',
+  lightGray: '#CCCCCC',
+  gray: '#999999',
+  darkGray: '#5B5B5B',
+  dark: '#4C4C4C',
+  red: '#c73838',
+}
